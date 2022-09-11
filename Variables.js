5c87479773c830c6d3bfd71e16d816c6f487d34f
Govind maithil <govindmaithilrtn@gmail.com>
2022-09-11 10:18:26 +0530

    JavaScript Variables

--- Variables.js
+++ Variables.js
@@ -1,1 +1,89 @@
-console.log("JavaScript_Basics");
+// --------> Var 
+
+
+// var myName = "govind"
+// console.log(myName);
+
+
+// var myName        ----- it can we declared without initialization.
+// myName = "Govind"
+// console.log(myName);
+
+// Global scope of ----- var -----
+
+// var name = "Sonu pal"
+// function myFunc (){
+//     console.log(name);
+// }
+// myFunc()
+
+// function scope  -- of var ----
+
+// function myFunc(){
+//     var myName = "Govind Maithil"
+//     console.log(myName);
+// }
+// myFunc()
+// console.log(myName);
+
+// Block Level Scope ---- of Var ---
+
+// if(true){
+//     var name = "Govind"
+//     console.log(name);
+// }
+// console.log(name);
+
+
+
+// ----------> Let 
+
+// let myName = "govind"
+// console.log(myName);
+
+
+// let myName        ----- it can we declared without initialization.
+// myName = "Govind"
+// console.log(myName);
+
+// Global scope of ----- let -----
+// let name = "Sonu pal"
+// function myFunc (){
+//     console.log(name);
+// }
+// myFunc()
+
+
+
+//  function scope  -- of Let ----
+
+// function myFunc(){
+//     let myName = "Govind Maithil singh"
+//     console.log(myName);
+// }
+// myFunc()
+// console.log(myName);  ----- we can not access out the outside of the function let have block level scope
+
+
+// ----------> Const 
+
+// const myName = "govind"
+// console.log(myName);
+
+
+// const myName        ----- it can not we declared without initialization.
+// myName = "Govind"  ------ error
+// console.log(myName);
+
+
+//  function scope  -- of const ----
+
+// function myFunc(){
+//     const myName = "Govind Maithil singh"
+//     console.log(myName);
+// }
+// myFunc()
+// console.log(myName);  ----- we can not access out the outside of the function let have block level scope
+
+
+
