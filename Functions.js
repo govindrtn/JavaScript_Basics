770e12e0e3ef8348ec3d6fa3ec45990b9dc7621e
Govind maithil <govindmaithilrtn@gmail.com>
2022-09-11 10:41:54 +0530

    JavaScript Function regular function and Arrow function and Anoymous function

---
+++ Functions.js
@@ -0,0 +1,31 @@
+// Regular Function  --------------------------->
+ 
+// function addTwo(a,b){
+//     console.log(a+b);
+// }
+// addTwo(43,33)
+
+// Arrow Function ------------------------------>
+
+// let addTwo = (a,b) =>{console.log(a+b);}
+// addTwo(54,56)
+
+// let addTwo = (a,b) => console.log(a+b)
+// addTwo(54,56)
+
+
+// Anonymous Function ---------------------------->
+// let addTwo = function(a,b){
+//     console.log("Anonymous function");
+//     console.log(a+b);
+// }
+// // calling Anonymous Function
+// addTwo(54,44)
+
+
+let addTwo = (a,b)=>{
+    console.log("Anonymous function");
+    console.log(a+b);
+}
+// calling Anonymous Function
+addTwo(54,45)
