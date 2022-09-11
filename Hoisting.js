a700d0a7e67d4d10fe401b5fafc1ffe225ef87a1
Govind maithil <govindmaithilrtn@gmail.com>
2022-09-11 10:23:50 +0530

    JavaScript Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,38 @@
+// console.log(name) undefined
+// var name
+
+
+// var name
+// console.log(name)  undefined
+  
+// console.log(name);  ---- undefined
+// name = "govind"
+// console.log(name);   
+// var name 
+
+
+// console.log(myname);  -- --- undefined
+// var myname = "sumit"
+
+
+
+// function greet(){
+//     name = "Govind"
+//     console.log(name);
+//     var name
+// }
+// greet()
+
+// function hoisting
+
+// myFunc()
+// function myFunc(){
+//     console.log("hello world");
+// }
+
+
+
+myFunc2()
+let myFunc2 = function (){
+    console.log("hello world")
+}
